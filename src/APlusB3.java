07a5f7a547d1ad49f532c88fca580c80e449fc0f
hantaehee <xogmlwkdi@naver.com>
2023-04-27 22:51:48 +0900

    10950번 A+B -3

---
+++ src/APlusB3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+/*10950번 A+B -3*/
+public class APlusB3 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+
+		for (int i = 0; i < a; i++) {
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			System.out.println(x+y);
+		}
+		sc.close();
+
+	}
+}
