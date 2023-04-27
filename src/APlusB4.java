9dc37d61830a9dd8ce7c085866f57264b1172417
hantaehee <xogmlwkdi@naver.com>
2023-04-27 22:51:59 +0900

    10951번 A+B -4

---
+++ src/APlusB4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/*10951번 A+B -4*/
+public class APlusB4 {
+	public static void main(String[] args) {
+		int x;
+		int y;
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNext()) {
+			x = sc.nextInt();
+			y = sc.nextInt();
+
+			System.out.println(x + y);
+		}
+
+		sc.close();
+	}
+
+}
