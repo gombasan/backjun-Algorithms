02e432385ad79b703fdf55bb6a1881ddcfae0c4a
hantaehee <xogmlwkdi@naver.com>
2023-04-27 22:53:28 +0900

    10807번 개수 세기

---
+++ src/Counting.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+/*10807번 개수 세기*/
+public class Counting {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int result = 0;
+		int num = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int current = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < num; i++) {
+			int temp = Integer.parseInt(st.nextToken());
+
+			if (temp == current) {
+				result++;
+			}
+		}
+		bw.write(String.valueOf(result));
+		bw.flush();
+
+		bw.close();
+		br.close();
+
+	}
+
+}
