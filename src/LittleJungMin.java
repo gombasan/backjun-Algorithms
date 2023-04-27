811215bb768c5935924d5d46dbc63f444837de4c
hantaehee <xogmlwkdi@naver.com>
2023-04-27 22:54:00 +0900

    11382번 꼬마정민

---
+++ src/LittleJungMin.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/*11382번 꼬마정민*/
+public class LittleJungMin {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long A = sc.nextLong();
+		long B = sc.nextLong();
+		long C = sc.nextLong();
+
+		System.out.println(A+B+C);
+		sc.close();
+	}
+}
