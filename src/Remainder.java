c32d6cb7baed9027e9c754249df176d7ffca47c1
hantaehee <xogmlwkdi@naver.com>
2023-04-27 22:54:44 +0900

    3052번 나머지

---
+++ src/Remainder.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+/*3052번 나머지*/
+public class Remainder {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int[] arr = new int[10];
+		boolean[] isSame = new boolean[10];
+		for (int i = 0; i < 10; i++) {
+			int num = Integer.parseInt(br.readLine());
+			arr[i] = num % 42;
+
+			for (int j = 0; j < i; j++) {
+				if(arr[j] == arr[i]) {
+					isSame[i] = true;
+				}
+			}
+
+		}
+		int result = 10;
+		for (int i = 0; i < 10; i++) {
+			if(isSame[i]) {
+				result--;
+			}
+		}
+		bw.write(String.valueOf(result));
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
