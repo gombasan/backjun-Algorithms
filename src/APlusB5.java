d2ca756c2450f2423b0647381f49fc44424d27b9
hantaehee <xogmlwkdi@naver.com>
2023-04-27 22:52:11 +0900

    10952번 A+B -5

---
+++ src/APlusB5.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.StringTokenizer;
+/*10952번 A+B -5*/
+public class APlusB5 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		while (true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			if (x + y == 0) {
+				break;
+			}
+			sb.append(x + y).append("\n");
+		}
+
+		bw.write(String.valueOf(sb));
+		bw.flush();
+
+		bw.close();
+		br.close();
+	}
+
+}
