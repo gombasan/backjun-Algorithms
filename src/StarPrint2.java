7e9584f7e0d3326bc4b370436595effa5438aa32
hantaehee <xogmlwkdi@naver.com>
2023-04-27 22:55:06 +0900

    2439번 별찍기 -2

---
+++ src/StarPrint2.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+/*2439번 별찍기 -2*/
+public class StarPrint2 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		int num = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= num; i++) {
+
+
+			for (int j = 0; j < num - i; j++) {
+				sb.append(" ");
+			}
+
+			for (int k = 0; k < i; k++) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+		bw.write(String.valueOf(sb));
+		bw.flush();
+
+		bw.close();
+		br.close();
+	}
+
+}
